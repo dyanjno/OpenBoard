28a6bdee0b3f82cdb09f5886dd6fdc9ff40bb5c0
dyanjno <pulkitgupta.2018@gmail.com>
2022-08-27 09:30:50 +0530

    update

--- public/js/tool.js
+++ public/js/tool.js
@@ -1,5 +1,5 @@
 // connect to socket server
-//const socket = io.connect("https://pepboard.herokuapp.com/");
+const socket = io.connect("https://my-openboard-project.herokuapp.com/");
 // *********************************Basic Setup
 const board = document.querySelector(".board");
 board.height = window.innerHeight;
